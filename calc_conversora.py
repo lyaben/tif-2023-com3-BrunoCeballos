827080c5e04eb216044e9f1f204e06f27fb1bb64
BrunoCeballos <136517079+BrunoCeballos@users.noreply.github.com>
2023-06-19 21:43:43 -0300

    Versión 1.1

--- calc_conversora.py
+++ calc_conversora.py
@@ -1,3 +1,6 @@
+#Calculadora Conversora Trabajo Grupal Final - Ceballos - Yaben
+
+#Función que verifica que la entrada sea un entero positivo
 def es_int(valor):
     try:
         valor = int(valor)
@@ -9,7 +12,8 @@ def es_int(valor):
         else:
             valido = False
     return valido
-    
+
+#Función que verifica que la opción elegida sea válida    
 def op_val(opcion):
     if opcion == '1':
         valido2 = True
@@ -20,7 +24,8 @@ def op_val(opcion):
     else:
         valido2 = False
     return valido2
-    
+
+#Función que convierte un número en base decimal en un string en base 2 u 8 y lo imprime
 def conv_bin_oct(numero, base):
     orig = numero
     resul = ""
@@ -30,22 +35,25 @@ def conv_bin_oct(numero, base):
     resul = str(numero) + resul
     print (f"{orig} en base {base} es: {resul}")
 
+#Función que convierte digitos en base decimal a caracteres en base hexadecimal
 def dig_hex(digito):
     if digito < 10:
-        return str(digito)
+        hexa = str(digito)
     elif digito == 10:
-        return 'A'
+        hexa = 'A'
     elif digito == 11:
-        return 'B'
+        hexa = 'B'
     elif digito == 12:
-        return 'C'
+        hexa = 'C'
     elif digito == 13:
-        return 'D'
+        hexa = 'D'
     elif digito == 14:
-        return 'E'
+        hexa = 'E'
     elif digito == 15:
-        return 'F'
-    
+        hexa = 'F'
+    return hexa
+
+#Función que convierte un número en base decimal en un sting en base 16 y lo imprime
 def conv_hex(numero):
     orig = numero
     resul = ""
@@ -54,22 +62,23 @@ def conv_hex(numero):
         numero = numero//16
     resul = dig_hex(numero) + resul
     print (f"{orig} en base 16 es: {resul}")
-        
+
+#Cuerpo principal de la calculadora, se ejecuta desde el selector         
 def conversora():
-    numero = input("Ingrese el número a convertir: ")
+    numero = input("Ingrese el número a convertir: ")   #Pido el número a convertir, verificando que sea un entero positivo
     while not es_int(numero):
         numero = input("Ingrese un valor válido (entero positivo): ")
     numero = int(numero)
-    print (" ___________________________________________ \n"
+    print (" ___________________________________________ \n"    #Imprimo la interfaz de selección
         "|Seleccione base a la que quiere convertir: |\n"
         "|1. Bin                                     |\n"
         "|2. Hex                                     |\n"
         "|3. Oct                                     |\n"
         "|___________________________________________|\n")
-    op = input()
-    while not op_val(op):
+    op = input()                    #Ingreso la opción elegida
+    while not op_val(op):           #Verifico que la opción ingresada sea válida
         op = input("Seleccione una opción válida (1-3): ")
-    if op == '1':
+    if op == '1':                   #Derivo la opción a la función correspondiente
         conv_bin_oct(numero, 2)
     elif op == '2':
         conv_hex(numero)
